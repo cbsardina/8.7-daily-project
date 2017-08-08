e274e43169947033b020d2e7561f58af4a9400cd
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-07 21:40:25 -0400

    8/7 Daily Proj: Layed out. Final styling to be completed.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let main = document.querySelector('.main');
+let vCard = document.querySelector('.vCard');
 
 let request = new XMLHttpRequest();
 request.addEventListener("load", getProfileData);
@@ -6,34 +6,37 @@ request.open('GET', 'https://api.github.com/users/cbsardina');
 request.send();
 
 function getProfileData(request) {
-  let list = '';
   let data = JSON.parse(this.responseText);
 
     function populatePage(data){
-
         const myProfile = `
+            <div class="main">
             <header>
               <h1>${data.name}</h1>
             </header>
-            <div class="details">
-              <h2>The Basics</h2>
-              <ul>
-                <li><span>Name: </span>${data.name}</li>
-                <li><span>GitHub URL: </span>${data.html_url}</li>
-                <li><span>Email: </span>${data.login}@gmail.com</li>
-                <li><span>Company: </span>${data.company}</li>
-                <li><span>Website: </span>${data.blog}</li>
-              </ul>
+            <div class="border">&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;&#x25BC;</div>
+              <div class="details">
+                <h2>The Basics</h2>
+                <ul>
+                  <li><span>Name: </span>${data.name}</li>
+                  <li><span>GitHub URL: </span>${data.html_url}</li>
+                  <li><span>Email: </span>${data.login}@gmail.com</li>
+                  <li><span>Company: </span>${data.company}</li>
+                  <li><span>Website: </span>${data.blog}</li>
+                </ul>
+              </div>
+              <section class="story">
+                <h2>The Story</h2>
+                <p>${data.bio}</p>
+                </section>
+                <div class="profilePic">
+                  <img src="${data.avatar_url}" alt="Puppy monkey baby">
+                </div>
             </div>
-            <section>
-              <h2>The Story</h2>
-              <p>${data.bio}</p>
-              <img src="${data.avatar_url}" alt="Puppy monkey baby">
-            </section>
             `
             return myProfile;
           }
           let addToHTML = populatePage(data);
 
-          main.innerHTML = addToHTML;
+          vCard.innerHTML = addToHTML;
 }
